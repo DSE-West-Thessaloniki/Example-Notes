3459cf94f5ebef89d9b9aa09e797385a97ec8a7e
Theofilos Intzoglou <int.teo@gmail.com>
2022-05-29 20:12:36 +0000

    Δημιουργία view για την λίστα των σημειώσεων

---
+++ resources/ts/Pages/Note/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Σημειώσεις</h1>
+        <ul v-if="notes">
+            <li v-for="note in notes" :key="note.id">
+                <div class="h2">{{ note.title }}</div>
+                <div>{{ note.content }}</div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    notes: Array<App.Models.Note>
+}>();
+</script>
