31dae164c0a3a7a0d5ff26a037f26ba331ff94d1
Theofilos Intzoglou <int.teo@gmail.com>
2022-05-30 11:56:00 +0000

    Υλοποίηση σύνδεσης με όνομα χρήστη αντί email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory()
+            ->state(['username' => 'admin'])
+            ->hasNotes(30)
+            ->create();
+        User::factory()
             ->count(10)
             ->hasNotes(30)
             ->create();
--- resources/ts/Pages/Auth/Register.vue
+++ resources/ts/Pages/Auth/Register.vue
@@ -1,64 +1,74 @@
 <template>
-  <Head title="Register" />
 
-  <jet-authentication-card>
-    <template #logo>
-      <jet-authentication-card-logo />
-    </template>
+    <Head title="Register" />
 
-    <div class="card-body">
+    <jet-authentication-card>
+        <template #logo>
+            <jet-authentication-card-logo />
+        </template>
 
-      <jet-validation-errors class="mb-3" />
+        <div class="card-body">
 
-      <form @submit.prevent="submit">
-        <div class="mb-3">
-          <jet-label for="name" value="Name" />
-          <jet-input id="name" type="text" v-model="form.name" required autofocus autocomplete="name" />
+            <jet-validation-errors class="mb-3" />
+
+            <form @submit.prevent="submit">
+                <div class="mb-3">
+                    <jet-label for="name" value="Name" />
+                    <jet-input id="name" type="text" v-model="form.name" required autofocus autocomplete="name" />
+                </div>
+
+                <div class="mb-3">
+                    <jet-label for="username" value="Username" />
+                    <jet-input id="username" type="username" v-model="form.username" required />
+                </div>
+
+                <div class="mb-3">
+                    <jet-label for="email" value="Email" />
+                    <jet-input id="email" type="email" v-model="form.email" required />
+                </div>
+
+                <div class="mb-3">
+                    <jet-label for="password" value="Password" />
+                    <jet-input id="password" type="password" v-model="form.password" required
+                        autocomplete="new-password" />
+                </div>
+
+                <div class="mb-3">
+                    <jet-label for="password_confirmation" value="Confirm Password" />
+                    <jet-input id="password_confirmation" type="password" v-model="form.password_confirmation" required
+                        autocomplete="new-password" />
+                </div>
+
+                <div class="mb-3" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
+                    <div class="custom-control custom-checkbox">
+                        <jet-checkbox name="terms" id="terms" v-model:checked="form.terms" />
+
+                        <label class="custom-control-label" for="terms">
+                            I agree to the <a target="_blank" :href="route('terms.show')">Terms of Service</a> and <a
+                                target="_blank" :href="route('policy.show')">Privacy Policy</a>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="mb-0">
+                    <div class="d-flex justify-content-end align-items-baseline">
+                        <Link :href="route('login')" class="text-muted me-3 text-decoration-none">
+                        Already registered?
+                        </Link>
+
+                        <jet-button class="ms-4" :class="{ 'text-white-50': form.processing }"
+                            :disabled="form.processing">
+                            <div v-show="form.processing" class="spinner-border spinner-border-sm" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+
+                            Register
+                        </jet-button>
+                    </div>
+                </div>
+            </form>
         </div>
-
-        <div class="mb-3">
-          <jet-label for="email" value="Email" />
-          <jet-input id="email" type="email" v-model="form.email" required />
-        </div>
-
-        <div class="mb-3">
-          <jet-label for="password" value="Password" />
-          <jet-input id="password" type="password" v-model="form.password" required autocomplete="new-password" />
-        </div>
-
-        <div class="mb-3">
-          <jet-label for="password_confirmation" value="Confirm Password" />
-          <jet-input id="password_confirmation" type="password" v-model="form.password_confirmation" required autocomplete="new-password" />
-        </div>
-
-        <div class="mb-3" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
-          <div class="custom-control custom-checkbox">
-            <jet-checkbox name="terms" id="terms" v-model:checked="form.terms" />
-
-            <label class="custom-control-label" for="terms">
-              I agree to the <a target="_blank" :href="route('terms.show')">Terms of Service</a> and <a target="_blank" :href="route('policy.show')">Privacy Policy</a>
-            </label>
-          </div>
-        </div>
-
-        <div class="mb-0">
-          <div class="d-flex justify-content-end align-items-baseline">
-            <Link :href="route('login')" class="text-muted me-3 text-decoration-none">
-              Already registered?
-            </Link>
-
-            <jet-button class="ms-4" :class="{ 'text-white-50': form.processing }" :disabled="form.processing">
-              <div v-show="form.processing" class="spinner-border spinner-border-sm" role="status">
-                <span class="visually-hidden">Loading...</span>
-              </div>
-
-              Register
-            </jet-button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </jet-authentication-card>
+    </jet-authentication-card>
 </template>
 
 <script>
@@ -73,36 +83,37 @@ import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
 import { Head, Link } from '@inertiajs/inertia-vue3'
 
 export default defineComponent({
-  components: {
-    Head,
-    JetAuthenticationCard,
-    JetAuthenticationCardLogo,
-    JetButton,
-    JetInput,
-    JetCheckbox,
-    JetLabel,
-    JetValidationErrors,
-    Link,
-  },
+    components: {
+        Head,
+        JetAuthenticationCard,
+        JetAuthenticationCardLogo,
+        JetButton,
+        JetInput,
+        JetCheckbox,
+        JetLabel,
+        JetValidationErrors,
+        Link,
+    },
 
-  data() {
-    return {
-      form: this.$inertia.form({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-        terms: false,
-      })
-    }
-  },
+    data() {
+        return {
+            form: this.$inertia.form({
+                name: '',
+                username: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
+                terms: false,
+            })
+        }
+    },
 
-  methods: {
-    submit() {
-      this.form.post(this.route('register'), {
-        onFinish: () => this.form.reset('password', 'password_confirmation'),
-      })
+    methods: {
+        submit() {
+            this.form.post(this.route('register'), {
+                onFinish: () => this.form.reset('password', 'password_confirmation'),
+            })
+        }
     }
-  }
 })
 </script>
