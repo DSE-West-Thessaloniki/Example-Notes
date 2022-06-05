e640e152f928ae8e255f19bf1b6943902116a9d9
Theofilos Intzoglou <int.teo@gmail.com>
2022-06-05 13:34:09 +0000

    Υλοποίηση σελιδοποίησης των σημειώσεων

--- resources/ts/Pages/Note/Index.vue
+++ resources/ts/Pages/Note/Index.vue
@@ -8,7 +8,7 @@
                 <input type="text" name="filter" v-model="filter" class="col-sm-3" placeholder="Αναζήτηση..." />
             </div>
             <ul v-if="notes">
-                <li v-for="note in notes" :key="note.id">
+                <li v-for="note in notes.data" :key="note.id">
                     <div class="h2">{{ note.title }}
                         <Link class="fs-6" :href="route('note.edit', note)">Επεξεργασία</Link>
                         <Link class="fs-6 px-1" :href="route('note.destroy', note)" method="delete">Διαγραφή</Link>
@@ -17,19 +17,27 @@
                     <div>{{ note.content }}</div>
                 </li>
             </ul>
+            <nav v-if="notes.next_page_url !== notes.prev_page_url">
+                <ul class="pagination">
+                    <li v-for="link in notes.links" :class="pageItemClass(link)">
+                        <Link class="page-link" :href="link.url" v-html="link.label">
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </MainLayout>
 </template>
 <script setup lang="ts">
 import MainLayout from '@/Layouts/MainLayout.vue';
-import { Link } from '@inertiajs/inertia-vue3';
+import { InertiaLink, Link } from '@inertiajs/inertia-vue3';
 import route from 'ziggy-js';
 import { ref, watch } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import throttle from 'lodash/throttle';
 
 const props = defineProps<{
-    notes: Array<App.Models.Note>,
+    notes: { data: Array<App.Models.Note>, links: Array<InertiaLink>, next_page_url: string, prev_page_url: string },
     filters: { "filter": string }
 }>();
 
@@ -41,4 +49,16 @@ watch(filter, throttle(value => {
         replace: true
     })
 }, 500));
+
+function pageItemClass(item: InertiaLink) {
+    if (item.url) {
+        if (item.active) {
+            return "page-item active";
+        }
+
+        return "page-item";
+    }
+
+    return "page-item disabled";
+}
 </script>
